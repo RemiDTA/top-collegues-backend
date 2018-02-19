2d6a50a465857309bd60f8cd8570f96cb184560e
Remi <ferreira-remi@hotmail.fr>
2018-02-19 14:07:55 +0100

    back ok

--- src/main/java/dev/controller/collegueController.java
+++ src/main/java/dev/controller/collegueController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import dev.repository.CollegueRepository;
 
 @RestController
 @RequestMapping("/collegues")
+@CrossOrigin
 public class collegueController {
 	@Autowired
 	CollegueRepository collegueRepo;
